948c69c6e2eebdb8f4efef56328d64f13e464c1c
Gionata Mettifogo <gionata@Yoshi.local>
2021-12-24 13:16:40 +0100

    Add strapi CSP policy

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,11 +1,25 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      // https://www.npmjs.com/package/strapi-provider-upload-google-cloud-storage
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "storage.googleapis.com"],
+          "media-src": ["'self'", "data:", "blob:", "storage.googleapis.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::favicon",
+  "strapi::public",
 ];
