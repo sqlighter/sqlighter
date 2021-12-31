497810417c3a1e2d32fd20632e20d0dc52a05849
Gionata Mettifogo <gionata@Yoshi.local>
2021-12-31 14:39:11 +0100

    Remove strapi

--- backend/src/api/supercazzola/routes/supercazzola.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * supercazzola router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::supercazzola.supercazzola');
