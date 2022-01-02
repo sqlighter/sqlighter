6ee2b9040cf7e8c0385fa9e8b12ccf337fae9181
Gionata Mettifogo <gionata@Yoshi.local>
2022-01-02 13:24:02 +0100

    Draft code to import biomarkers from json

--- backend/extensions/endpoints/insieme/index.js
+++ backend/extensions/endpoints/insieme/index.js
@@ -2,6 +2,10 @@
 
 var biomarkers = require('../../../../docs/biomarkers/biomarker-types.json');
 
+function ifNotNull(value) {
+    return value && value != "NULL" ? value : null
+}
+
 var route = (router, { services, exceptions }) => {
 	const { ItemsService } = services;
 	const { ServiceUnavailableException } = exceptions;
@@ -21,43 +25,64 @@ var route = (router, { services, exceptions }) => {
 		res.json({ version: 3 });
 	});
 
-	router.get('/initialize', async (req, res, next) => {
+	router.get('/setup-biomarkers', async (req, res, next) => {
 		const unitsService = new ItemsService('units', { schema: req.schema, accountability: req.accountability });
+		const biomarkersService = new ItemsService('biomarkers', {
+			schema: req.schema,
+			accountability: req.accountability,
+		});
 
+		// find unique units
 		const units = {};
 		biomarkers.forEach((biomarker) => {
 			if (biomarker.Units) {
 				units[biomarker.Units] = null;
 			}
 		});
-		//        console.log(units);
 
-        var manyUnits = await unitsService.readMany();
-        res.json(manyUnits);
-
-        for(var unit in units) {
+		// create any missing measurement units
+		var allUnits = await unitsService.readMany();
+		for (var unit in units) {
 			console.log(unit);
+			var oldUnit = allUnits.find((existingUnit) => existingUnit.id.toLowerCase() == unit.toLowerCase());
+			if (!oldUnit) {
+				console.log(`will insert new unit ${unit}`);
+				var newUnit = await unitsService.createOne({
+					id: unit,
+				});
+			}
+		}
 
-            var oldUnit = await unitsService.readOne(unit);
-			console.log(oldUnit);
-            if (!oldUnit) {
-                var newUnit = await unitsService.createOne({
-                    id: unit,
-                })
-            }
-        }
+		// create missing biomarkers
+		var allBiomarkers = await biomarkersService.readMany();
+		for (var biomarkerIdx in biomarkers) {
+            var biomarker = biomarkers[biomarkerIdx]
+			console.log(unit);
+			var oldBiomarker = allBiomarkers.find(
+				(existingBiomarker) => existingBiomarker.id.toLowerCase() == biomarker.Code.toLowerCase()
+			);
+			if (!oldBiomarker) {
 
-//		Object.keys(units).forEach(async (unit) => {
-//		});
 
-		const biomaService = new ItemsService('posts', { schema: req.schema, accountability: req.accountability });
+				console.log(`will insert new biomarker ${biomarker.Code}`);
+				var newBiomarker = await biomarkersService.createOne({
+					id: biomarker.Code.toLowerCase(),
+					units: ifNotNull(biomarker.Units),
+					risks: ifNotNull(biomarker.Risk),
+					translations: [
+						{
+							languages_code: 'en-US',
+							name: ifNotNull(biomarker.Name),
+                            description: ifNotNull(biomarker.Description),
+                            summary: ifNotNull(biomarker.Summary)
+						},
+					],
+				});
+			}
+		}
 
-		biomarkers.forEach((biomarker) => {
-			//            console.log(biomarker.Code);
-		});
-
-		res.json(biomarkers);
-        
+        allBiomarkers = await biomarkersService.readMany();
+		res.json(allBiomarkers);
 	});
 };
 
