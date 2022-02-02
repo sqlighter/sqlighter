42fe1f94808b6b0bf9037fb6ecc0ad9b7457eb7a
Gionata Mettifogo <gmettifogo@gmail.com>
2022-02-02 09:22:02 +0100

    Create signin.tsx

---
+++ app/pages/signin.tsx
@@ -0,0 +1,50 @@
+import Layout from "../components/layout"
+import Head from "next/head"
+import Date from "../components/date"
+import utilStyles from "../styles/utils.module.css"
+import { GetStaticProps, GetStaticPaths } from "next"
+import { Biomarker } from "../lib/biomarkers"
+import Link from "next/link"
+import Script from "next/script"
+
+export default function SigninPage({ biomarkers, locale }: { biomarkers: Biomarker[]; locale: string }) {
+  return (
+    <Layout>
+      <Head>
+        <title>Signin</title>
+      </Head>
+      <Script
+        src="https://accounts.google.com/gsi/client"
+        strategy="lazyOnload"
+        onLoad={() => {
+          function handleCredentialResponse(response) {
+            console.log("Encoded JWT ID token: " + response.credential)
+          }
+          google.accounts.id.initialize({
+            client_id: "427320365950-75nnbuht76pb6femtq9ccctqhs0a4qbb.apps.googleusercontent.com",
+            callback: handleCredentialResponse,
+          })
+          google.accounts.id.renderButton(
+            document.getElementById("buttonDiv"),
+            { theme: "outline", size: "large" } // customization attributes
+          )
+          google.accounts.id.prompt() // also display the One Tap dialog
+        }}
+      />
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <div id="buttonDiv"></div>
+      </section>
+    </Layout>
+  )
+}
+
+/** Static properties from biomarkers.json */
+export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
+  return {
+    props: {
+      biomarkers: "ciao",
+      locale,
+    },
+  }
+}
