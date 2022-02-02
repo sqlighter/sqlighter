344e810d6e9fcc3c0451022becbe8f9d19d9813a
Gionata Mettifogo <gmettifogo@gmail.com>
2022-02-02 10:51:11 +0100

    Decode jwt token

--- app/pages/signin.tsx
+++ app/pages/signin.tsx
@@ -7,8 +7,31 @@ import { Biomarker } from "../lib/biomarkers"
 import Link from "next/link"
 import Script from "next/script"
 
-function onGoogleSigninLoad(ctx) {
-  console.log(`onGoogleSigninLoad`, ctx)
+import jwt_decode from "jwt-decode"
+
+function onGoogleSignin(response) {
+  const jwtToken = response.credential
+  var decoded = jwt_decode(jwtToken)
+  console.log(`onGoogleSignin`, response, decoded)
+}
+
+async function onGoogleLoad(params) {
+  const google = (window as any).google
+  console.log(`onGoogleLoad`, params, google)
+
+  google.accounts.id.initialize({
+    client_id: "427320365950-75nnbuht76pb6femtq9ccctqhs0a4qbb.apps.googleusercontent.com",
+    callback: onGoogleSignin,
+  })
+
+  // https://developers.google.com/identity/gsi/web/guides/personalized-button
+  google.accounts.id.renderButton(
+    document.getElementById("buttonDiv"),
+    { theme: "outline", size: "large" } // customization attributes
+  )
+
+  // also display the One Tap dialog
+  google.accounts.id.prompt()
 }
 
 export default function SigninPage({ biomarkers, locale }: { biomarkers: Biomarker[]; locale: string }) {
@@ -18,28 +41,7 @@ export default function SigninPage({ biomarkers, locale }: { biomarkers: Biomark
         <title>Signin</title>
       </Head>
 
-      <Script
-        src="https://accounts.google.com/gsi/client"
-        strategy="lazyOnload"
-        onLoad={(params) => {
-          console.log(params)
-          const google = (window as any).google
-          console.log(google)
-
-          function handleCredentialResponse(response) {
-            console.log("Encoded JWT ID token: " + response.credential)
-          }
-          google.accounts.id.initialize({
-            client_id: "427320365950-75nnbuht76pb6femtq9ccctqhs0a4qbb.apps.googleusercontent.com",
-            callback: handleCredentialResponse,
-          })
-          google.accounts.id.renderButton(
-            document.getElementById("buttonDiv"),
-            { theme: "outline", size: "large" } // customization attributes
-          )
-          google.accounts.id.prompt() // also display the One Tap dialog
-        }}
-      />
+      <Script src="https://accounts.google.com/gsi/client" strategy="lazyOnload" onLoad={onGoogleLoad} />
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <div id="buttonDiv"></div>
