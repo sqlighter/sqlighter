d921312d88a11f82060554f0e35661f88c09027d
Gionata Mettifogo <gmettifogo@gmail.com>
2022-02-03 13:46:33 +0100

    Updated terms and privacy stub pages

--- app/pages/privacy.tsx
+++ app/pages/privacy.tsx
@@ -2,15 +2,19 @@ import Layout from "../components/layout"
 import Head from "next/head"
 import utilStyles from "../styles/utils.module.css"
 
-export default function TermsPage(props) {
+export default function PrivacyPolicyPage(props) {
   return (
     <Layout>
       <Head>
         <title>Privacy Policy</title>
       </Head>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        Privacy policy, this site is under construction.
-      </section>
+      <h2>Privacy Policy</h2>
+      <p>
+        This site uses <a href="https://developers.google.com/identity/gsi/web">Google Identity Services</a> for the
+        purpose of signing you in without having to handle or store your credentials. Data provided to this site is
+        exclusively used to support signing in and is not passed to any third party services, other than for the
+        purposes of authentication.
+      </p>
     </Layout>
   )
 }
