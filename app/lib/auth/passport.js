15501cd63a89e41cea303a41c79075d361e0ea40
Gionata Mettifogo <gmettifogo@gmail.com>
2022-02-05 09:56:28 +0100

    Implement passport signin to user in database

--- app/lib/auth/passport.js
+++ app/lib/auth/passport.js
@@ -1,31 +1,34 @@
-import passport from "passport"
-import LocalStrategy from "passport-local"
-import { findUserByUsername, validatePassword } from "../db"
-import { GoogleOneTapStrategy } from "passport-google-one-tap"
-
-import { writeJson } from "../utilities"
-
-passport.serializeUser(function (user, done) {
-  console.log(`serializeUser`, user)
-  // serialize the username into session
-  done(null, user.email)
-})
-
-passport.deserializeUser(function (req, id, done) {
-  // deserialize the username back into user object
-  console.log(`deserializeUser`, id)
-  // const user = findUserByUsername(req, id)
-  done(null, { email: id })
-})
-
-// https://www.passportjs.org/packages/passport-google-one-tap/
-
-// Configure the Google One Tap strategy for use by Passport.
 //
-// Google One Tap strategy require a `verify` function which receives the
-// with the user's profile. The function must invoke `done`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
+// passport.js - configures Passport auth for Google One Tap Signin
+//
+
+import passport from "passport"
+import { GoogleOneTapStrategy } from "passport-google-one-tap"
+import { User } from "../users"
+
+/** Serialize a user object into its email which is used as id in sessions */
+passport.serializeUser(function (user, done) {
+  // console.debug(`serializeUser - ${user.id}`)
+  done(null, user.id)
+})
+
+/** Deserialize user's email (used as id) back into user's record */
+passport.deserializeUser(async (req, id, done) => {
+  const user = await User.getUser(id)
+  if (!user) {
+    console.warn(`deserializeUser - could not find user for email: ${id}`)
+  }
+  // console.debug(`deserializeUser - ${id}`)
+  done(null, user)
+})
+
+/**
+ * Configure the Google One Tap strategy for use by Passport.
+ * Google One Tap strategy require a `verify` function which receives the
+ * with the user's profile. The function must invoke `done` with a user
+ * object, which will be set at `req.user` in route handlers after authentication.
+ * @see https://www.passportjs.org/packages/passport-google-one-tap/
+ */
 passport.use(
   new GoogleOneTapStrategy(
     {
@@ -33,30 +36,15 @@ passport.use(
       consumerSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
     async function (profile, done) {
-      console.log(`GoogleOneTapStrategy`, profile)
-      await writeJson("profile.json", profile)
-      if (!profile) {
-        return done(undefined, undefined)
+      try {
+        const user = await User.signinUser(profile)
+        return done(undefined, user)
+      } catch (exception) {
+        console.warn(`GoogleOneTapStrategy - an error occoured while signing in ${exception}`, exception)
+        return done(exception, undefined)
       }
-      //      return done(undefined, { email: "gmettifogo@gmail.com" })
-      profile.email = profile.emails[0].value
-      return done(undefined, profile)
     }
   )
 )
-/*
-passport.use(
-  new LocalStrategy({ passReqToCallback: true }, (req, username, password, done) => {
-    // Here you lookup the user in your DB and compare the password/hashed password
-    const user = findUserByUsername(req, username)
-    // Security-wise, if you hashed the password earlier, you must verify it
-    // if (!user || await argon2.verify(user.password, password))
-    if (!user || !validatePassword(user, password)) {
-      done(null, null)
-    } else {
-      done(null, user)
-    }
-  })
-)
-*/
+
 export default passport
---
+++ app/lib/users.ts
@@ -0,0 +1,66 @@
+//
+// users.ts - user profiles and authentication information
+//
+
+import { Item } from "./items"
+import { items } from "./database"
+
+export const USER_TYPE = "user"
+export const USER_PREFIX = "usr_"
+
+/** A generic item with tree structure and open ended attributes */
+export class User extends Item {
+  constructor() {
+    super()
+    this.type = USER_TYPE
+  }
+
+  /**
+   * Retrieves user from database if found
+   * @param userId Normally the user's email address
+   */
+  static async getUser(userId: string): Promise<User | null> {
+    const item = await items.selectItem(userId)
+    if (item) {
+      return Object.assign(new User(), item)
+    }
+    return null
+  }
+
+  /**
+   * Called when signing in using passport or other OAuth2 login provider.
+   * This method will
+   * @param id The user id, normally an email address (could be a normalized phone later)
+   * @param profile The normalized profile for the user retrieved by signin provider
+   * @returns A user, will create a new user if needed
+   * @see https://www.passportjs.org/reference/normalized-profile/
+   * @see https://datatracker.ietf.org/doc/html/draft-smarr-vcarddav-portable-contacts-00
+   */
+  static async signinUser(profile) {
+    // for now we use user's email as id, later we may use phone number as well
+    const email = profile?.emails?.[0]?.value
+    console.debug(`User.signinUser - ${email}`, profile)
+    if (!email || email.indexOf("@") == -1) {
+      console.warn(`User.signinUser - profile is missing valid email`, profile)
+      throw new Error("User profile doesn't have an email address")
+    }
+
+    let user = await User.getUser(email)
+    if (!user) {
+      // create a new user record with fresh profile
+      user = new User()
+      user.id = email
+      user.attributes.passport = profile
+      await items.insertItem(user)
+    } else {
+      // update existing user record
+      user.attributes.passport = profile
+      await items.updateItem(user)
+    }
+
+    // return with updated timestamps
+    return await User.getUser(user.id)
+  }
+}
+
+export default User
