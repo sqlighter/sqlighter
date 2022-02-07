aebe052dbf5966d5dba0964178000d0f56e53194
Gionata Mettifogo <gmettifogo@gmail.com>
2022-02-07 18:50:13 +0100

    Add footer with main bottom navigation

---
+++ app/components/footer.tsx
@@ -0,0 +1,38 @@
+//
+// footer.tsx - implements main bottom navigation
+//
+
+import { useRouter } from "next/router"
+
+import * as React from "react"
+import Paper from "@mui/material/Paper"
+import BottomNavigation from "@mui/material/BottomNavigation"
+import BottomNavigationAction from "@mui/material/BottomNavigationAction"
+
+import JournalIcon from "@mui/icons-material/AssignmentOutlined"
+import BrowseIcon from "@mui/icons-material/FormatListBulletedOutlined"
+import ProfileIcon from "@mui/icons-material/PersonOutlineOutlined"
+
+const Footer = () => {
+  const router = useRouter()
+
+  // if pathname is '/browse/biomarkers/glucose' pathSegments[0] will be 'browse'
+  console.assert(router.pathname.length > 1 && router.pathname.startsWith("/"))
+  let pathSegments = router.pathname.length > 1 && router.pathname.substring(1).split("/")
+
+  function navigateTo(event, newPath: string) {
+    router.push("/" + newPath)
+  }
+
+  return (
+    <Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }} elevation={3}>
+      <BottomNavigation value={pathSegments[0]} onChange={navigateTo} showLabels>
+        <BottomNavigationAction label="Journal" value="journal" icon={<JournalIcon />} />
+        <BottomNavigationAction label="Browse" value="browse" icon={<BrowseIcon />} />
+        <BottomNavigationAction label="Profile" value="profile" icon={<ProfileIcon />} />
+      </BottomNavigation>
+    </Paper>
+  )
+}
+
+export default Footer
