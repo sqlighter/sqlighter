e29a0bc527165937592ee832a00119b5ea0f2ae0
Gionata Mettifogo <gmettifogo@gmail.com>
2022-02-11 11:15:12 +0100

    Section variant

--- app/components/section.tsx
+++ app/components/section.tsx
@@ -6,15 +6,20 @@ import Typography from "@mui/material/Typography"
 
 interface SectionProps {
   title?: string
-  subtitle?: string
+  subtitle?: string,
+  large?: boolean
   children: any
 }
 
-export function Section({ title, subtitle, children }: SectionProps) {
+export function Section({ title, subtitle, large, children }: SectionProps) {
   return (
     <section>
-      {title && <Typography variant="h4">{title}</Typography>}
-      {subtitle && <Typography variant="body2">{subtitle}</Typography>}
+      {title && <Typography variant={large ? "h2" : "h4"}>{title}</Typography>}
+      {subtitle && (
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {subtitle}
+        </Typography>
+      )}
       {children}
     </section>
   )
