41448621235e5610c0562350680560035853c3e1
Gionata Mettifogo <gmettifogo@gmail.com>
2022-02-11 11:15:28 +0100

    Simple typography test page

---
+++ app/pages/lorem.tsx
@@ -0,0 +1,61 @@
+import { GetStaticProps } from "next"
+import Head from "next/head"
+import Link from "next/link"
+import utilStyles from "../styles/utils.module.css"
+
+import * as React from "react"
+import Avatar from "@mui/material/Avatar"
+import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+import Stack from "@mui/material/Stack"
+import Tooltip from "@mui/material/Tooltip"
+
+import { useUser } from "../lib/auth/hooks"
+import { Context } from "../components/context"
+import Layout from "../components/layout"
+import { Typography } from "@mui/material"
+
+import { SigninPanel } from "../components/signin"
+import { Section } from "../components/section"
+
+export default function LoremPage(props) {
+  const context = React.useContext(Context)
+
+  return (
+    <Layout title="Lorem">
+      <Section large title="This is a section" subtitle="This is the sections's subtitle">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In neque ex, feugiat ut posuere eget, interdum ut
+          est. Nam vitae dui aliquet, elementum augue nec, fringilla arcu. Suspendisse ut fringilla sem. Sed aliquam
+          pretium commodo. Proin pharetra varius odio in condimentum. Mauris et nisi malesuada, commodo augue eget,
+          rhoncus magna.
+        </p>
+        {["First", "Second", "Third", "Fourth", "Fifth"].map((section) => {
+          return (
+            <Section title={section + " subsection"} subtitle="Also has a subtitle">
+              <p>
+                Vivamus consequat neque tellus, vitae pretium lacus interdum et. Vestibulum ante ipsum primis in
+                faucibus orci luctus et ultrices posuere cubilia curae; Praesent sollicitudin tincidunt ullamcorper. Sed
+                eu eros placerat, faucibus nisi et, pharetra nisl. Sed et est vehicula, semper nunc sit amet, feugiat
+                dolor.
+              </p>
+              <p>
+                Vestibulum rhoncus, felis quis scelerisque convallis, urna urna lobortis velit, id viverra leo turpis in
+                leo. Donec sit amet tellus odio. Praesent mattis tincidunt magna sit amet viverra. Maecenas imperdiet mi
+                et nisi bibendum finibus. Aenean sit amet nunc eleifend eros euismod vehicula sollicitudin non ante.
+              </p>
+            </Section>
+          )
+        })}
+      </Section>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      //
+    },
+  }
+}
