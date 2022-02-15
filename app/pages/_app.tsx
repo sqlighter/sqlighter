a647bb3cfe060f20ec0226e38973ad01bb02af16
Gionata Mettifogo <gmettifogo@gmail.com>
2022-02-15 13:23:41 +0100

    Update crossOrigin tag

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -232,7 +232,7 @@ export default function App({ Component, pageProps }) {
               <meta name="theme-color" content={PRIMARY_LIGHTEST} />
 
               <link rel="preconnect" href="https://fonts.googleapis.com" />
-              <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+              <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
               <link
                 href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap"
                 rel="stylesheet"
