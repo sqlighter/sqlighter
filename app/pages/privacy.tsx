c218d6a10c56de47564f757bd340ac3181b7996e
Gionata Mettifogo <gmettifogo@gmail.com>
2022-02-15 19:01:35 +0100

    Privacy, terms using Layout with title

--- app/pages/privacy.tsx
+++ app/pages/privacy.tsx
@@ -3,15 +3,32 @@ import Head from "next/head"
 
 export default function PrivacyPolicyPage() {
   return (
-    <Layout>
-      <Head>
-        <title>Privacy Policy</title>
-      </Head>
-      <h2>Privacy Policy</h2>
+    <Layout title="Privacy Policy" subtitle="We respect your privacy">
+      <h4>Information We Collect</h4>
       <p>
-        This site uses <a href="https://developers.google.com/identity/gsi/web">Google Identity Services</a> for the
-        purpose of signing you in without having to handle or store your credentials. Data provided to this site is used
-        exclusively to give personalized tips and recommendations and is never passed to any third party services.
+        Your privacy is important to us. It is Insieme&#39;s policy to respect your privacy and comply with any
+        applicable law and regulation regarding any personal information we may collect about you. Information we
+        collect includes both information you knowingly and actively provide us when using or participating in any of
+        our services, and any information automatically sent by your devices in the course of accessing
+        our services.
+      </p>
+      <h4>Personal Information</h4>
+      <p>
+        We may ask for personal information which may include one or more of the following: Name, email, date of birth,
+        sex, values of your biomarkers, etc. We only collect and use your personal information when we have a legitimate
+        reason for doing so. In which instance, we only collect personal information that is reasonably necessary to
+        provide our services so that we can customise or personalise your experience of our website.
+      </p>
+      <h4>Security of Your Personal Information</h4>
+      <p>
+        When we collect and process personal information, and while we retain this information, we will protect it
+        within commercially acceptable means to prevent loss and theft, as well as unauthorized access, disclosure,
+        copying, use, or modification.
+      </p>
+      <h4>Contact Us</h4>
+      <p>
+        For any questions or concerns regarding your privacy, you may contact us using the following details:
+        <a href="mailto:privacy@insieme.app">privacy@insieme.app</a>.
       </p>
     </Layout>
   )
--- app/pages/terms.tsx
+++ app/pages/terms.tsx
@@ -3,18 +3,12 @@ import Head from "next/head"
 
 export default function TermsOfServicePage() {
   return (
-    <Layout>
-      <Head>
-        <title>Terms of Service</title>
-      </Head>
-      <h2>Terms of Service</h2>
-      <p>
+    <Layout title="Terms of Service">
         THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
         LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
         SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
         OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
         DEALINGS IN THE SOFTWARE.
-      </p>
     </Layout>
   )
 }
