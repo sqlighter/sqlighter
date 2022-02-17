f1fa93431164b720d978ef1e4d7526b53807f94c
Gionata Mettifogo <gmettifogo@gmail.com>
2022-02-17 22:02:15 +0100

    Reorder elements, rough style

--- app/components/articlelistitem.tsx
+++ app/components/articlelistitem.tsx
@@ -5,10 +5,12 @@
 import Link from "next/link"
 import Image from "next/image"
 
+import Box from "@mui/material/Box"
 import ListItem from "@mui/material/ListItem"
 import ListItemButton from "@mui/material/ListItemButton"
 import ListItemText from "@mui/material/ListItemText"
 import ListItemAvatar from "@mui/material/ListItemAvatar"
+import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography"
 
 import { Logo } from "./logo"
@@ -27,6 +29,8 @@ export interface ArticleListItemProps {
 export function ArticleListItem(props: ArticleListItemProps) {
   console.assert(props.url)
 
+  const hostname = new URL(props.url).hostname
+
   return (
     <Link href={props.url} passHref>
       <ListItemButton
@@ -35,9 +39,14 @@ export function ArticleListItem(props: ArticleListItemProps) {
         className="article-listitem"
       >
         <ListItem alignItems="flex-start" disableGutters dense={true} sx={{ marginTop: -1, marginBottom: -1 }}>
-          <Typography variant="h4">{props.title}</Typography>
-          <img src={props.imageUrl} alt={props.title} className="article-thumbnail" />
-          <Logo organizationId={props.organizationId} height={20} width={200} />
+          <Box display="flex">
+            <Stack sx={{ flexGrow: 1 }}>
+              <Logo organizationId={props.organizationId} height={20} width={200} />
+              <Typography variant="body1" color="text.primary">{props.title}</Typography>
+              <Typography variant="caption" color="text.secondary">{hostname}</Typography>
+            </Stack>
+            <img src={props.imageUrl} alt={props.title} className="article-thumbnail" />
+          </Box>
         </ListItem>
       </ListItemButton>
     </Link>
