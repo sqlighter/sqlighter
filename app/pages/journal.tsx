86dce9f926cb79412796b99de35c10c00bc8d0f2
Gionata Mettifogo <gmettifogo@gmail.com>
2022-03-08 14:57:54 +0100

    Yarn reinstall

--- app/pages/journal.tsx
+++ app/pages/journal.tsx
@@ -151,8 +151,10 @@ export default function JournalPage(props: JournalPageProps) {
   )
 }
 
+/*
 export async function getServerSideProps(context) {
   return {
     props: {}, // will be passed to the page component as props
   }
 }
+*/
