a699342e49e996331c219725c2e2d06d4ccfcacb
Gionata Mettifogo <gmettifogo@gmail.com>
2022-03-10 16:46:38 +0100

    Draft journal entries with chips, placeholder page for records

--- app/pages/journal.tsx
+++ app/pages/journal.tsx
@@ -6,6 +6,13 @@ import React, { useContext, useCallback, useState } from "react"
 import Box from "@mui/system/Box"
 import Typography from "@mui/material/Typography"
 
+import Timeline from "@mui/lab/Timeline"
+import TimelineItem from "@mui/lab/TimelineItem"
+import TimelineSeparator from "@mui/lab/TimelineSeparator"
+import TimelineConnector from "@mui/lab/TimelineConnector"
+import TimelineContent from "@mui/lab/TimelineContent"
+import TimelineDot from "@mui/lab/TimelineDot"
+
 import { generateId } from "../lib/items/items"
 import { AppLayout } from "../components/layouts"
 import { SigninPanel } from "../components/signin"
@@ -14,6 +21,114 @@ import { Empty } from "../components/empty"
 import emptyImage from "../public/images/empty1.jpg"
 import { useApi } from "../lib/api"
 import { UploadButton } from "../components/upload"
+import { TimelineOppositeContent } from "@mui/lab"
+import Chip from "@mui/material/Chip"
+import Badge from "@mui/material/Badge"
+import IconButton from "@mui/material/IconButton"
+
+import AttachmentIcon from "@mui/icons-material/FilePresentOutlined"
+import InsertDriveFileOutlinedIcon from "@mui/icons-material/InsertDriveFileOutlined"
+import VideoFileOutlinedIcon from "@mui/icons-material/VideoFileOutlined"
+import AudioFileOutlinedIcon from "@mui/icons-material/AudioFileOutlined"
+
+function FileIcon({ contentType }) {
+  switch (contentType) {
+    case "application/pdf":
+      return <AttachmentIcon fontSize="large" />
+  }
+  return <InsertDriveFileOutlinedIcon sx={{ fontSize: 64 }} color="secondary" />
+}
+
+function JournalEntryFiles({ item }) {
+  return (
+    <>
+      {item.files.map((file) => {
+        return (
+          <Box key={file.id}>
+            <FileIcon contentType={file.contentType} />
+            <Typography variant="caption">{JSON.stringify(file)}</Typography>
+            {file.id}
+          </Box>
+        )
+      })}
+    </>
+  )
+}
+
+function JournalEntryContent({ item }) {
+  function handleBiomarkerClick(measurement) {
+    console.debug(`JournalEntryContent.handleBiomarkerClick - ${measurement.biomarker}`, measurement)
+  }
+
+  return (
+    <Box mb={4}>
+      <Typography variant="body1">{item.title || item.id}</Typography>
+      <Typography variant="subtitle2" color="text.secondary">
+        {item.createdAt}
+      </Typography>
+      {item.description && (
+        <Typography variant="body1" color="text.primary">
+          {item.description}
+        </Typography>
+      )}
+
+      {item.measurements && (
+        <>
+          <Typography variant="overline">Biomarkers</Typography>
+          <Box display="flex" width="100%" flexWrap="wrap">
+            {item.measurements.map((measurement) => {
+              const biomarkerUrl = `/${item.type}s/${item.id}#${measurement.biomarker}`
+              return (
+                <Box mr={1} mb={1}>
+                  <Badge
+                    color="secondary"
+                    variant="dot"
+                    overlap="circular"
+                    anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                  >
+                    <Chip
+                      key={measurement.biomarker}
+                      variant="outlined"
+                      component="a"
+                      href={biomarkerUrl}
+                      onClick={(e) => handleBiomarkerClick(measurement)}
+                      label={measurement.biomarker}
+                    />{" "}
+                  </Badge>
+                </Box>
+              )
+            })}
+          </Box>
+        </>
+      )}
+      {item.files && (
+        <>
+          <Typography variant="overline">Documents</Typography>
+          <JournalEntryFiles item={item} />
+        </>
+      )}
+    </Box>
+  )
+}
+
+function JournalEntry({ item }) {
+  return (
+    <TimelineItem>
+      <TimelineOppositeContent
+        style={{ maxWidth: "1px", paddingLeft: "0px", paddingRight: "0px" }}
+      ></TimelineOppositeContent>
+      <TimelineSeparator color="secondary">
+        <TimelineDot color="secondary">
+          <InsertDriveFileOutlinedIcon fontSize="small" />
+        </TimelineDot>
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent sx={{ width: 600 }}>
+        <JournalEntryContent item={item} />
+      </TimelineContent>
+    </TimelineItem>
+  )
+}
 
 interface JournalPageProps {
   /** List of available health records */
@@ -40,6 +155,15 @@ export default function JournalPage(props: JournalPageProps) {
     return (
       <Box width="100%" overflow="hidden">
         <UploadButton itemType="record" itemId={itemId} onUploaded={handleUploaded} onProgress={handleUploadProgress} />
+
+        <Timeline position="right" sx={{ paddingLeft: 0 }}>
+          {records &&
+            records.map((item) => {
+              return <JournalEntry key={item.id} item={item} />
+            })}
+          ì{" "}
+        </Timeline>
+
         {records &&
           records.map((record) => {
             return (
---
+++ app/pages/records/[recordId].tsx
@@ -0,0 +1,33 @@
+//
+// /records/[recordId].tsx - biomarker detail page
+//
+
+import { GetServerSidePropsContext } from "next"
+import { ContentPage } from "../../components/contentpage"
+import { Biomarker } from "../../lib/items/biomarkers"
+import { getSerializableContent } from "../../lib/props"
+import { ItemsTable } from "../../lib/database"
+
+export default function RecordPage({ item }: { item: Biomarker }) {
+  if (!item.title) {
+    item.title = item.id
+  }
+
+  // TODO prepare contents before passing to content page
+
+  return <ContentPage item={item} />
+}
+
+/** Dynamic properties from /records/recordId */
+export async function getServerSideProps({ req, params, locale }: GetServerSidePropsContext) {
+  const itemsTable = new ItemsTable()
+  const recordId = params.recordId as string
+  const record = await itemsTable.selectItem(recordId)
+  const item = await getSerializableContent(record, true, true)
+  console.debug(`${req.url} - recordId: ${recordId}`, item)
+
+  return {
+    // will be passed to the page component as props
+    props: { item }, 
+  }
+}
