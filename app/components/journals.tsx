63f2aec2db43f558c7bfa00060d8dbcc422e7106
Gionata Mettifogo <gmettifogo@gmail.com>
2022-03-14 09:33:58 +0100

    Primary color

--- app/components/journals.tsx
+++ app/components/journals.tsx
@@ -3,8 +3,9 @@
 //
 
 import React from "react"
-import Link from "next/link"
+import NextLink from "next/link"
 
+import Link from "@mui/material/Link"
 import Box from "@mui/system/Box"
 import Typography from "@mui/material/Typography"
 import Tooltip from "@mui/material/Tooltip"
@@ -13,13 +14,14 @@ import Badge from "@mui/material/Badge"
 import Button from "@mui/material/Button"
 import ButtonBase from "@mui/material/ButtonBase"
 import IconButton from "@mui/material/IconButton"
+
 import AttachmentIcon from "@mui/icons-material/FilePresentOutlined"
 import InsertDriveFileOutlinedIcon from "@mui/icons-material/InsertDriveFileOutlined"
 import VideoFileOutlinedIcon from "@mui/icons-material/VideoFileOutlined"
 import AudioFileOutlinedIcon from "@mui/icons-material/AudioFileOutlined"
 import FileDownloadIcon from "@mui/icons-material/FileDownloadOutlined"
-import ScienceIcon from '@mui/icons-material/ScienceOutlined';
-import FileIcon from '@mui/icons-material/InsertDriveFileOutlined';
+import ScienceIcon from "@mui/icons-material/ScienceOutlined"
+import FileIcon from "@mui/icons-material/InsertDriveFileOutlined"
 
 import Timeline from "@mui/lab/Timeline"
 import TimelineItem from "@mui/lab/TimelineItem"
@@ -58,20 +60,27 @@ export function JournalEntry({ icon, title, description, actions, href, children
     <TimelineItem>
       <TimelineOppositeContent style={{ maxWidth: 0, paddingLeft: 0, paddingRight: 0 }} />
       <TimelineSeparator color="primary">
-        <TimelineDot variant="outlined" sx={{ color: "primary.light", borderColor: "primary.light" }}>
+        <TimelineDot variant="outlined" sx={{ color: "primary.main", borderColor: "primary.main" }}>
           {icon ? icon : <FileIcon />}
         </TimelineDot>
-        <TimelineConnector sx={{ bgcolor: "primary.light" }} />
+        <TimelineConnector sx={{ bgcolor: "primary.main" }} />
       </TimelineSeparator>
       <TimelineContent sx={{ width: 240, marginRight: 0, paddingRight: 0 }}>
         <Box mb={6}>
           <Box display="flex">
             <Box width="100%" flexGrow={1}>
-              {title && (
-                <Typography variant="h3" color="text.primary" noWrap={true}>
-                  {title}
-                </Typography>
-              )}
+              {title &&
+                (href ? (
+                  <NextLink href={href} passHref={true}>
+                    <Link variant="h3" underline="hover" noWrap={true} href={href}>
+                      {title}
+                    </Link>
+                  </NextLink>
+                ) : (
+                  <Typography variant="h3" color="primary" noWrap={true}>
+                    {title}
+                  </Typography>
+                ))}
               {description && (
                 <Typography variant="subtitle2" color="text.secondary" noWrap={true}>
                   {description}
@@ -93,7 +102,7 @@ export function JournalEntry({ icon, title, description, actions, href, children
   )
 
   if (href) {
-    entry = <Link href={href}>{entry}</Link>
+    //    entry = <Link href={href}>{entry}</Link>
   }
 
   return entry
