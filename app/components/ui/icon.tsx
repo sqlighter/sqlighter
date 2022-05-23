1814c8ad31beddfaf7fe9f404fbd28397eb1003b
Gionata Mettifogo <gmettifogo@gmail.com>
2022-05-23 11:16:09 +0200

    Add icons for database

--- app/components/ui/icon.tsx
+++ app/components/ui/icon.tsx
@@ -5,16 +5,25 @@
 import * as React from "react"
 import { SvgIconProps } from "@mui/material"
 
-// for now we import icons one by one to try keeping bundle size down
-// https://materialdesignicons.com/
-// https://fontawesome.com/icons/server?s=regular
-// https://pictogrammers.github.io/@mdi/font/6.5.95/
-
+// Material Icons (Google)
+// https://mui.com/material-ui/material-icons/?query=table&theme=Outlined
+// https://fonts.google.com/icons
 import DatabaseIcon from "@mui/icons-material/StorageOutlined"
 import FileIcon from "@mui/icons-material/InsertDriveFileOutlined"
 import FolderIcon from "@mui/icons-material/FolderOutlined"
 import TableIcon from "@mui/icons-material/TableChartOutlined"
+import KeyIcon from "@mui/icons-material/KeyOutlined"
 import QuestionMarkIcon from "@mui/icons-material/QuestionMarkOutlined"
+import BoltOutlinedIcon from "@mui/icons-material/BoltOutlined"
+
+// Material Design Icons (open source)
+// https://materialdesignicons.com/
+// https://github.com/TeamWertarbyte/mdi-material-ui
+// https://pictogrammers.github.io/@mdi/font/6.5.95/
+import { DatabaseOutline } from "mdi-material-ui"
+
+// Font Awesome
+// https://fontawesome.com/icons/server?s=regular
 
 export interface IconProps extends SvgIconProps {
   children: string | React.ReactNode
@@ -24,12 +33,17 @@ export function Icon(props: IconProps) {
   if (typeof props.children === "string") {
     switch (props.children) {
       case "database":
-        return <DatabaseIcon {...props} />
+        return <DatabaseOutline {...props} />
       case "file":
         return <FileIcon {...props} />
       case "folder":
         return <FolderIcon {...props} />
+      case "key":
+        return <KeyIcon {...props} />
+      case "trigger":
+        return <BoltOutlinedIcon {...props} />
       case "table":
+      case "view":
         return <TableIcon {...props} />
     }
     return <QuestionMarkIcon {...props} />
