5653fe2ee767fa394fb3531ae1e91d95053d87b4
Gionata Mettifogo <gmettifogo@gmail.com>
2022-05-25 13:01:24 +0200

    Split TreeItem component to its own file

--- app/components/navigation/treeview.tsx
+++ app/components/navigation/treeview.tsx
@@ -4,37 +4,18 @@
 
 import * as React from "react"
 import { useState } from "react"
-
 import Box from "@mui/material/Box"
-import ButtonBase from "@mui/material/ButtonBase"
-import Chip from "@mui/material/Chip"
-import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography"
-import Tooltip from "@mui/material/Tooltip"
-import IconButton from "@mui/material/IconButton"
-
 import { SxProps, Theme } from "@mui/material"
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
-import ArrowRightIcon from "@mui/icons-material/ArrowRight"
 
 import { useSettings } from "../hooks/useSettings"
-import { Command } from "../../lib/data/commands"
 import { Tree } from "../../lib/data/tree"
-import { Icon } from "../ui/icon"
+import { TreeItem } from "./treeitem"
 
-// delay before showing tooltips
-const TOOLTIP_ENTER_DELAY_MS = 1000
-
+/** Custom styles applied to TreeView and TreeItem components */
 const TREEVIEW_STYLES: SxProps<Theme> = {
   width: "100%",
   height: "100%",
-
-  ".MuiChip-root": {
-    borderRadius: "4px",
-    ".MuiChip-label": {
-      fontSize: "0.6rem",
-    },
-  },
 
   ".TreeItem-root": {
     width: "100%",
@@ -92,10 +73,6 @@ const TREEVIEW_STYLES: SxProps<Theme> = {
       overflow: "hidden",
     },
 
-    ".MuiChip-root": {
-      cursor: "pointer",
-    },
-
     ".TreeItem-commands": {
       height: 18,
     },
@@ -125,6 +102,14 @@ const TREEVIEW_STYLES: SxProps<Theme> = {
   ".TreeItem-selected": {
     backgroundColor: "primary",
   },
+
+  ".MuiChip-root": {
+    cursor: "pointer",
+    borderRadius: "4px",
+    ".MuiChip-label": {
+      fontSize: "0.6rem",
+    },
+  },
 }
 
 const TREEITEM_NORESULTS_STYLES: SxProps = {
@@ -133,159 +118,6 @@ const TREEITEM_NORESULTS_STYLES: SxProps = {
   lineHeight: "24px",
   color: "text.secondary",
 }
-
-//
-// TreeItem
-//
-
-// pixels of indentation for each hierarchical level
-const DEPTH_PADDING_PX = 12
-
-export interface TreeItemProps {
-  /** Item and children to be shown */
-  item: Tree
-
-  /** Depth in tree data (null or zero for root) */
-  depth?: number
-
-  /** True if item is rendered as expanded */
-  expanded?: boolean
-
-  /** True if item should be rendered as selected */
-  selected?: boolean
-
-  /** True if item should be rendered as pinned */
-  pinned?: boolean
-
-  /** Callback used when one of the action or "collapse/expand" icons is clicked */
-  onCommand?: (event: React.SyntheticEvent, command: string, args) => void
-}
-
-//
-// TreeItem subcomponent
-//
-
-/** A component showing a tree item and its children (used by TreeView) */
-function TreeItem({ item, ...props }: TreeItemProps) {
-  /** An item is collapsible if it has an array of children (even if empty for now) */
-  function isCollapsible(): boolean {
-    return Array.isArray(item.children)
-  }
-
-  //
-  // handlers
-  //
-
-  function handleItemClick(e) {
-    console.debug("TreeItem.handleItemClick")
-    if (isCollapsible()) {
-      props.onCommand(e, props.expanded ? "sqlighter.collapseItem" : "sqlighter.expandItem", { item })
-    }
-  }
-
-  //
-  // render
-  //
-
-  const depthPadding = `${(props.depth || 0) * DEPTH_PADDING_PX}px`
-  let itemClass = "TreeItem-root"
-  if (props.depth == 0) {
-    itemClass += " TreeItem-primary"
-  }
-
-  function getCollapsibleIcon() {
-    if (isCollapsible()) {
-      if (props.expanded) {
-        return <ArrowDropDownIcon className="TreeItem-collapsibleIcon" />
-      } else {
-        return <ArrowRightIcon className="TreeItem-collapsibleIcon" />
-      }
-    }
-    return <Box className="TreeItem-collapsibleIcon" />
-  }
-
-  function getIcon() {
-    const icon = item.icon ? item.icon : isCollapsible() ? "folder" : "file"
-    return <Icon className="TreeItem-labelIcon">{icon}</Icon>
-  }
-
-  function getCommandIcon(command: Command) {
-    if (props.pinned) {
-      console.debug(`getCommandIcon - ${item.id} is pinned`)
-    }
-
-    let commandClass = "TreeItem-commandIcon"
-    let commandIcon = command.icon
-    if (props.pinned && command.command === "sqlighter.pin") {
-      commandClass += " TreeItem-pinnedIcon"
-      commandIcon = "pinned"
-    }
-
-    // NOTE the <div> inside Tooltip is necessary since Icon is a passive element that doesn't fire events (unlike IconButton)
-    return (
-      <Tooltip
-        key={command.command}
-        className="TreeItem-commandIconTooltip"
-        title={command.title}
-        placement="top"
-        enterDelay={TOOLTIP_ENTER_DELAY_MS}
-      >
-        <div>
-          <Icon
-            className={commandClass}
-            onClick={(e) => {
-              props.onCommand(e, command.command, { item, command })
-              e.stopPropagation()
-            }}
-          >
-            {commandIcon}
-          </Icon>
-        </div>
-      </Tooltip>
-    )
-  }
-
-  /** A tag is rendered as a chip with an optional tooltip */
-  function getTag(tag, index) {
-    if (typeof tag === "string") {
-      return <Chip key={index} className="TreeItem-tag" label={tag} size="small" />
-    }
-    return (
-      <Tooltip key={index} title={tag.tooltip} placement="top" enterDelay={TOOLTIP_ENTER_DELAY_MS}>
-        <Chip className="TreeItem-tag" label={tag.title} size="small" />
-      </Tooltip>
-    )
-  }
-
-  return (
-    <ButtonBase className={itemClass} onClick={handleItemClick}>
-      <Box className="TreeItem-depthPadding" sx={{ minWidth: depthPadding, width: depthPadding }} />
-      {getCollapsibleIcon()}
-      {item.icon && getIcon()}
-      <Typography className="TreeItem-label" variant="body2" color="inherit">
-        {item.title}
-        {item.badge !== null && item.badge !== undefined && (
-          <Chip className="TreeItem-badge" label={item.badge} size="small" />
-        )}
-      </Typography>
-      <Box sx={{ flexGrow: 1 }} />
-      {item.tags && (
-        <Stack className="TreeItem-tags" direction="row" spacing={0.5}>
-          {item.tags.map((tag, index) => getTag(tag, index))}
-        </Stack>
-      )}
-      {item.commands && (
-        <Stack className="TreeItem-commands" direction="row" spacing={0.5}>
-          {item.commands.map((command) => getCommandIcon(command))}
-        </Stack>
-      )}
-    </ButtonBase>
-  )
-}
-
-//
-// TreeView
-//
 
 export interface TreeViewProps {
   /** Items to be shown in tree view */
