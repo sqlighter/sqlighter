da986433d9d0d73ab848d1fca0479a3a81263459
Gionata Mettifogo <gmettifogo@gmail.com>
2022-05-26 22:36:17 +0200

    Add Google Analytics

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -122,6 +122,24 @@ export default function App({ Component, pageProps }: { Component: any; pageProp
 
               <link rel="preconnect" href="https://fonts.googleapis.com" />
               <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
             </Head>
             <Component {...pageProps} />
             <Script
