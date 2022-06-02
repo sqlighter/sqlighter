427cfc6a8e16b3ef86c12e9ba753947ab7c11d30
Gionata Mettifogo <gmettifogo@gmail.com>
2022-06-02 13:29:04 +0200

    Storybook for QueryRunPanel

---
+++ app/stories/fakedata.ts
@@ -0,0 +1,52 @@
+//
+// fakedata.ts
+//
+
+import { DataConnection } from "../lib/sqltr/connections"
+import { Query, QueryRun } from "../lib/items/query"
+import { addSeconds, subSeconds, parseISO, subMilliseconds } from "date-fns"
+
+// DataConnection (fake)
+export const fake_connection1 = {
+  id: "dbc_fake01",
+  title: "chinook.db",
+  configs: {
+    client: "sqlite3",
+  },
+} as DataConnection
+
+// DataConnection[]
+export const connections1: DataConnection[] = [fake_connection1]
+
+// Query (simple)
+export const fake_query1 = new Query()
+fake_query1.connectionId = fake_connection1.id
+fake_query1.sql = "SELECT\n  *\nFROM\n  albums"
+fake_query1.createdAt = parseISO("2022-06-02 07:02:15")
+
+// QueryRun (completed)
+export const fake_queryCompleted1 = new QueryRun()
+fake_queryCompleted1.query = fake_query1
+fake_queryCompleted1.sql = fake_query1.sql
+fake_queryCompleted1.status = "completed"
+fake_queryCompleted1.createdAt = parseISO("2022-06-02 13:02:15")
+fake_queryCompleted1.updatedAt = parseISO("2022-06-02 13:02:18.141592")
+fake_queryCompleted1.columns = ["FirstName", "LastName"]
+fake_queryCompleted1.values = [
+  ["John", "Jane"],
+  ["Smith", "Doe"],
+]
+
+// QueryRun (completed)
+export const fake_queryRunning1 = new QueryRun()
+fake_queryRunning1.query = fake_query1
+fake_queryRunning1.sql = fake_query1.sql
+fake_queryRunning1.status = "running"
+fake_queryRunning1.createdAt = new Date()
+
+// QueryRun (error)
+export const fake_queryError1 = new QueryRun()
+fake_queryError1.query = fake_query1
+fake_queryError1.sql = fake_query1.sql
+fake_queryError1.status = "error"
+fake_queryError1.error = 'error: Error: near "FROM1": syntax error'
---
+++ app/stories/queryrunpanel.stories.tsx
@@ -0,0 +1,47 @@
+//
+// queryrunpanel.stories.tsx
+//
+
+import React from "react"
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+import { Box } from "@mui/material"
+import { StorybookDecorator } from "../components/storybook"
+import { QueryRunPanel } from "../components/database/queryrunpanel"
+import { fake_connection1, fake_queryCompleted1, fake_queryRunning1, fake_queryError1 } from "./fakedata"
+
+export default {
+  title: "Components/QueryRunPanel",
+  component: QueryRunPanel,
+  decorators: [
+    (Story) => (
+      <StorybookDecorator>
+        <Story />
+      </StorybookDecorator>
+    ),
+  ],
+  args: {
+    connection: fake_connection1,
+    run: fake_queryCompleted1,
+  },
+} as ComponentMeta<typeof QueryRunPanel>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof QueryRunPanel> = (args) => (
+  <Box sx={{ height: 450, backgroundColor: "yellow" }}>
+    <QueryRunPanel {...args} />
+  </Box>
+)
+
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
+export const Completed = Template.bind({})
+
+export const Running = Template.bind({})
+Running.args = {
+  run: fake_queryRunning1,
+}
+
+export const Error = Template.bind({})
+Error.args = {
+  run: fake_queryError1,
+}
