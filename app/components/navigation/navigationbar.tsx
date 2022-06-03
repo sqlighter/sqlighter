64d17f4301d16dcec59c893e34e136c1df29765a
Gionata Mettifogo <gmettifogo@gmail.com>
2022-06-03 11:06:14 +0200

    Basic navigation bar in material 3 style

---
+++ app/components/navigation/navigationbar.tsx
@@ -0,0 +1,40 @@
+/**
+ * navigationbar.tsx - navigation bar composed of icon buttons possibly with a selected state
+ */
+
+import * as React from "react"
+import { SxProps, Theme } from "@mui/material"
+import Stack from "@mui/material/Stack"
+
+import { Command, CommandEvent } from "../../lib/commands"
+import { IconButton } from "../ui/iconbutton"
+
+const IconButton_SxProps: SxProps<Theme> = {
+  borderRadius: "8px",
+}
+
+export interface NavigationBarProps {
+  /** Commands rendered in this navigation bar */
+  commands: Command[]
+
+  /** Command that should be selected (optional) */
+  selected?: string
+
+  /** True if buttons titles should be shown next to icon (default false) */
+  label?: boolean
+
+  /** Command handler for button clicks */
+  onCommand?: CommandEvent
+}
+
+/** A navigation bar with icon buttons styled a la material design 3 */
+export function NavigationBar(props: NavigationBarProps) {
+  return (
+    <Stack direction="row">
+      {props.commands.map((cmd) => {
+        const selected = props.selected && cmd.command === props.selected
+        return <IconButton command={cmd} label={props.label} selected={selected} onCommand={props.onCommand} />
+      })}
+    </Stack>
+  )
+}
---
+++ app/stories/navigationbar.stories.tsx
@@ -0,0 +1,49 @@
+//
+// navigatiobar.stories.tsx
+//
+
+import React, { useState } from "react"
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+import { StorybookDecorator } from "../components/storybook"
+import { NavigationBar } from "../components/navigation/navigationbar"
+import { chartsCommand, databaseCommand, queryCommand } from "./fakedata"
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Components/NavigationBar",
+  component: NavigationBar,
+  decorators: [
+    (Story) => (
+      <StorybookDecorator>
+        <Story />
+      </StorybookDecorator>
+    ),
+  ],
+  args: {
+    commands: [databaseCommand, queryCommand, chartsCommand],
+    label: true,
+  },
+} as ComponentMeta<typeof NavigationBar>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof NavigationBar> = (args) => <NavigationBar {...args} />
+
+const SelectableTemplate: ComponentStory<typeof NavigationBar> = (args) => {
+  function SelectableBar(props) {
+    const [selected, setSelected] = useState(args.selected || args.commands[0].command)
+    function handleCommand(e, command) {
+      setSelected(command.command)
+    }
+    return <NavigationBar {...args} selected={selected} onCommand={handleCommand} />
+  }
+  return <SelectableBar />
+}
+
+export const Primary = Template.bind({})
+
+export const Selectable = SelectableTemplate.bind({})
+
+export const WithoutLabels = Template.bind({})
+WithoutLabels.args = {
+  label: false,
+}
