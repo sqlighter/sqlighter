50c162a2586dd7ceb8ea538a4ce629d7096ff020
Gionata Mettifogo <gmettifogo@gmail.com>
2022-06-04 14:28:55 +0200

    Create titlefield.tsx

---
+++ app/components/ui/titlefield.tsx
@@ -0,0 +1,74 @@
+//
+// titlefield.tsx
+//
+
+import React from "react"
+import { Theme, SxProps } from "@mui/material"
+import { TextField, OutlinedTextFieldProps } from "@mui/material"
+import { CommandEvent } from "../../lib/commands"
+
+// styles applied to main and subcomponents
+const TitleField_SxProps: SxProps<Theme> = {
+  //  height: 40,
+
+  // disable outline normally so it looks like a label, not an editfield
+  "& .MuiOutlinedInput-root": {
+    "& > fieldset": {
+      borderColor: "transparent",
+    },
+  },
+
+  // light outline on hover
+  "&:hover .MuiOutlinedInput-root": {
+    "& > fieldset": {
+      borderColor: "text.secondary",
+    },
+  },
+
+  "& .MuiOutlinedInput-root.Mui-focused": {
+    "& > fieldset": {
+      borderColor: "primary.main",
+    },
+  },
+}
+
+/** A text field used as editable title for panels */
+export interface TitleFieldProps {
+  /** Name to be assigned to this component (optional) */
+  className?: string
+  /** Value is required as title is a controlled component */
+  value?: string
+  /** Callback used to dispatch commands back to parent component */
+  onCommand?: CommandEvent
+}
+
+export function TitleField(props: TitleFieldProps) {
+  //
+  // handlers
+  //
+
+  function handleChange(event) {
+    console.debug(`TitleField.handleChange - ${event.target.value}`)
+    if (props.onCommand) {
+      props.onCommand(event, { command: "changeTitle", args: { item: event.target.value } })
+    }
+  }
+
+  //
+  // render
+  //
+
+  const className = props.className ? "TitleField-root " + props.className : "TitleField-root"
+  return (
+    <TextField
+      className={className}
+      value={props.value}
+      variant="outlined"
+      multiline={false}
+      fullWidth={true}
+      size="small"
+      onChange={handleChange}
+      sx={TitleField_SxProps}
+    />
+  )
+}
