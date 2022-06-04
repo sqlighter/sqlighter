9ddfad828976e7e60648b7e0dd6b96740404bfaa
Gionata Mettifogo <gmettifogo@gmail.com>
2022-06-04 14:00:25 +0200

    Add connection mock

--- app/stories/fakedata.ts
+++ app/stories/fakedata.ts
@@ -2,30 +2,48 @@
 // fakedata.ts
 //
 
-import { DataConnection } from "../lib/sqltr/connections"
+import { DataConnection, DataConnectionConfigs, DataSchema } from "../lib/sqltr/connections"
 import { Query, QueryRun } from "../lib/items/query"
 import { parseISO } from "date-fns"
 import { Command } from "../lib/commands"
 import fakeCustomers from "./data/customers"
-
-//import { readJson } from "../lib/utilities"
+import { QueryExecResult } from "sql.js"
 
 // DataConnection (fake)
-export const fake_connection1 = {
-  id: "dbc_fake01",
-  title: "chinook.db",
-  configs: {
-    client: "sqlite3",
-  },
-} as DataConnection
+class FakeConnection extends DataConnection {
+  id = "dbc_fake01"
+  title = "company.db"
+  
+  public constructor(configs: DataConnectionConfigs) {
+    super(configs)
+  }
+  public async getSchemas(refresh: boolean = false): Promise<DataSchema[]> {
+    return null // TODO
+  }
+  public async getResults(sql: string): Promise<QueryExecResult[]> {
+    return [fakeCustomers]
+  }
+  public async getResult(sql: string): Promise<QueryExecResult> {
+    return fakeCustomers
+  }
+  public async getRowsModified(): Promise<number> {
+    return 0
+  }
+}
+
+export const fake_connection1 = new FakeConnection({
+  client: "sqlite3",
+  connection: {},
+})
 
 // DataConnection[]
-export const connections1: DataConnection[] = [fake_connection1]
+export const fake_connections1: DataConnection[] = [fake_connection1]
 
 // Query (simple)
 export const fake_query1 = new Query()
 fake_query1.connectionId = fake_connection1.id
 fake_query1.sql = "SELECT\n  *\nFROM\n  customers"
+fake_query1.title = "All customers (from 2000)"
 fake_query1.createdAt = parseISO("2022-06-02 07:02:15")
 
 // QueryRun (completed)
---
+++ app/stories/querypanel.stories.tsx
@@ -0,0 +1,42 @@
+//
+// queryrunpanel.stories.tsx
+//
+
+import React from "react"
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+import { Box } from "@mui/material"
+import { StorybookDecorator } from "../components/storybook"
+import { QueryPanel } from "../components/database/querypanel"
+import { fake_connections1, fake_connection1, fake_query1 } from "./fakedata"
+
+export default {
+  title: "Components/QueryPanel",
+  component: QueryPanel,
+  decorators: [
+    (Story) => (
+      <StorybookDecorator>
+        <Story />
+      </StorybookDecorator>
+    ),
+  ],
+  args: {
+    connections: fake_connections1,
+    query: fake_query1,
+  },
+  parameters: {
+    grid: { cellSize: 8 },
+  },
+} as ComponentMeta<typeof QueryPanel>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof QueryPanel> = (args) => {
+  return (
+    <Box sx={{ height: 800, backgroundColor: "rgba(255,0,0,0.25)" }}>
+      <QueryPanel {...args} title={args.query.title} />
+    </Box>
+  )
+}
+
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
+export const Primary = Template.bind({})
