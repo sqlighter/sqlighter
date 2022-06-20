3661850ed7064f65f26ab243387a0b83cefa4e93
Gionata Mettifogo <gmettifogo@gmail.com>
2022-06-21 00:34:14 +0200

    Add TablePanel to storybook

---
+++ app/stories/tablepanel.stories.tsx
@@ -0,0 +1,67 @@
+//
+// tablepanel.stories.tsx
+//
+
+import React from "react"
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+import { StorybookDecorator, Wrapper } from "../components/storybook"
+import { TablePanel } from "../components/panels/tablepanel"
+import { getTestConnection } from "./fakedata"
+
+export default {
+  title: "Database/TablePanel",
+  component: TablePanel,
+  decorators: [
+    (Story) => (
+      <StorybookDecorator>
+        <Story />
+      </StorybookDecorator>
+    ),
+  ],
+  args: {
+    database: "main",
+    table: "customer",
+  },
+  parameters: {
+    grid: { cellSize: 8 },
+  },
+} as ComponentMeta<typeof TablePanel>
+
+const Template: ComponentStory<typeof TablePanel> = (args, { loaded: { connection, schemas } }) => {
+  return (
+    <Wrapper>
+      <TablePanel {...args} connection={connection} schema={schemas[0]} />
+    </Wrapper>
+  )
+}
+export const Primary = Template.bind({})
+Primary.loaders = [async () => await getTestConnection("Chinook.db")]
+Primary.args = {
+  table: "invoices",
+}
+
+export const WithNorthwind = Template.bind({})
+WithNorthwind.loaders = [async () => await getTestConnection("Northwind.db")]
+WithNorthwind.args = {
+  table: "employees",
+}
+
+export const WithNorthwindView = Template.bind({})
+WithNorthwindView.loaders = [async () => await getTestConnection("Northwind.db")]
+WithNorthwindView.args = {
+  table: "Invoices",
+  variant: "view",
+}
+
+export const WithSakila = Template.bind({})
+WithSakila.loaders = [async () => await getTestConnection("Sakila.db")]
+WithSakila.args = {
+  table: "film",
+}
+
+export const WithSakilaView = Template.bind({})
+WithSakilaView.loaders = [async () => await getTestConnection("Sakila.db")]
+WithSakilaView.args = {
+  table: "customer_list",
+  variant: "view",
+}
