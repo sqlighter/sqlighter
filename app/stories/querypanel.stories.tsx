be3478565a9561cdcdc1dfe23e80c4f91cfafd01
Gionata Mettifogo <gmettifogo@gmail.com>
2022-06-23 10:43:42 +0200

    Add QueryPanel stories with live database

--- app/stories/querypanel.stories.tsx
+++ app/stories/querypanel.stories.tsx
@@ -8,17 +8,17 @@ import { Box } from "@mui/material"
 import { StorybookDecorator } from "../components/storybook"
 import { QueryPanel } from "../components/panels/querypanel"
 import { fake_connections1, fake_query1 } from "./fakedata"
-// import { getChinookConnection } from "./fakedata"
+import { getTestConnection } from "./fakedata"
 
 export default {
-  title: "Database/QueryPanel",
+  title: "Panels/QueryPanel",
   component: QueryPanel,
   decorators: [
     (Story) => (
       <StorybookDecorator>
         <Story />
       </StorybookDecorator>
-    ), 
+    ),
   ],
   args: {
     connections: fake_connections1,
@@ -39,26 +39,44 @@ const Template: ComponentStory<typeof QueryPanel> = (args) => {
 
 export const Primary = Template.bind({})
 
-/* TODO figure out why this story doesn't run in github action
-const ChinookTemplate: ComponentStory<typeof QueryPanel> = (args, { loaded: { connections } }) => {
+// load database and schema asynchronously
+const chinookLoaders = [async () => await getTestConnection("Chinook.db")]
+const northwindLoaders = [async () => await getTestConnection("Northwind.db")]
+
+const LoadedTemplate: ComponentStory<typeof QueryPanel> = (args, { loaded: { connection } }) => {
+  console.debug(`ChinookTemplate - connection: ${connection}`, connection)
   return (
     <Box sx={{ height: 800, width: 1 }}>
-      <QueryPanel {...args} connections={connections} title={args.query.title} />
+      <QueryPanel {...args} connections={[connection]} title={args.query.title} />
     </Box>
   )
 }
-export const WithChinook = ChinookTemplate.bind({})
+export const WithChinook = LoadedTemplate.bind({})
+WithChinook.loaders = chinookLoaders
 WithChinook.args = {
   query: {
     connectionId: "dbc_chinook",
-    sql: "select * from albums",
-    title: "All chinook hits",
-    createdAt: new Date(),
+    sql: "SELECT * FROM albums",
+    title: "All hits",
+    createdAt: Date.parse("2022-06-21"),
   },
 }
-WithChinook.loaders = [
-  async () => ({
-    connections: [await getChinookConnection()],
-  }),
-]
-*/
+
+const NorthwindTemplate: ComponentStory<typeof QueryPanel> = (args, { loaded: { connection } }) => {
+  console.debug(`NorthwindTemplate - connection: ${connection}`, connection)
+  return (
+    <Box sx={{ height: 1200, width: 1 }}>
+      <QueryPanel {...args} connections={[connection]} title={args.query.title} />
+    </Box>
+  )
+}
+export const WithNorthwind = NorthwindTemplate.bind({})
+WithNorthwind.loaders = northwindLoaders
+WithNorthwind.args = {
+  query: {
+    connectionId: "dbc_northwind",
+    sql: 'SELECT * FROM Orders WHERE ShipCountry = "USA"',
+    title: "All domestic orders",
+    createdAt: Date.parse("2025-04-17"),
+  },
+}
