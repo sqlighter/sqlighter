2ee3445381871e895da908678a1cb3199eef6d80
Gionata Mettifogo <gmettifogo@gmail.com>
2022-06-25 08:37:25 +0200

    Storybook static config in ./storybook/main.js

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
+  staticDirs: [{ from: "../public", to: "/" }],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   framework: "@storybook/react",
   core: {
