325d26064f864aa45a552c638e53837a80c921fd
Gionata Mettifogo <gmettifogo@gmail.com>
2022-07-16 13:22:43 +0200

    Remove unused import

--- app/pages/terms.tsx
+++ app/pages/terms.tsx
@@ -4,7 +4,6 @@
 
 import Box from "@mui/material/Box"
 import Typography from "@mui/material/Typography"
-import Link from "@mui/material/Link"
 
 import { Area } from "../components/onepage/area"
 import { OnePageLayout } from "../components/onepage/onepagelayout"
