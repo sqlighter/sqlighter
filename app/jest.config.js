5b6b744cc40ef7067f6fbfec8569216c7db69f2d
Gionata Mettifogo <gmettifogo@gmail.com>
2023-09-05 13:19:17 +0200

    Single jest configuration

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,12 +1,17 @@
 // jest.config.js
-const nextJest = require('next/jest')
+const nextJest = require("next/jest")
 
 // Providing the path to your Next.js app which will enable loading next.config.js and .env files
-const createJestConfig = nextJest({ dir: './' })
+const createJestConfig = nextJest({ dir: "./" })
 
 // Any custom config you want to pass to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  reporters: ["default", "github-actions"],
+  testTimeout: 6000,
+  transform: {
+    "^.+\\.(t|j)sx?$": ["@swc-node/jest"],
+  },
 }
 
 // createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
